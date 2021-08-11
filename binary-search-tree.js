b6ef6616326a7ba40ffca2b76ed98bc76f5c2105
李豪珣 <519367854@qq.com>
2021-08-11 15:04:20 +0800

    添加二叉搜索树

---
+++ binary-search-tree.js
@@ -0,0 +1,16 @@
+class BinarySearchTree{
+    constructor(){
+
+    }
+}
+
+
+class Node{
+    constructor(key){
+        this.key = key;
+        // 左侧子节点引用
+        this.left = null; 
+        // 右侧子节点引用
+        this.right = null; 
+    }
+}
