b774fde432782e37aa2019be46dce8f356142096
李豪珣 <519367854@qq.com>
2021-08-11 22:36:24 +0800

    添加二叉搜索书的插入方法

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,6 +1,31 @@
 class BinarySearchTree{
-    constructor(){
 
+    constructor(){
+        this.root = null;
+    }
+
+    insert(key){
+        if(this.root == null){
+            this.root = new Node(key);
+        }else{
+            this.insertNode(this.root,key);
+        }
+    }
+
+    insertNode(node,key){
+        if(key < node.key){
+            if(node.left == null){
+                node.left = new Node(key);
+            }else{
+                this.insertNode(node.left,key);
+            }
+        }else{
+            if(node.right == null){
+                node.right = new Node(key);
+            }else{
+                this.insertNode(node.right,key);
+            }
+        }
     }
 }
 
